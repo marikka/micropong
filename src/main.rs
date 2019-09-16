035ded5b86b91ec9859b944bddf71bdabf79e39b
Marek Miettinen <marek.miettinen@gmail.com>
2019-09-15 23:32:59 -0400

    bounce direction based on collision

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use hal::i2c::I2c;
 use hal::stm32f0::stm32f0x2::I2C1;
 use stm32f0xx_hal as hal;
 
-use embedded_graphics::fonts::Font8x16;
+use embedded_graphics::fonts::Font6x12;
 use embedded_graphics::pixelcolor::PixelColorU8;
 use embedded_graphics::prelude::*;
 use embedded_graphics::primitives::{Circle, Rect};
@@ -21,6 +21,7 @@ use ssd1306::Builder;
 
 use arrayvec::ArrayString;
 use core::fmt::Write;
+use libm::F32Ext;
 
 const PADDLE_THICKNESS: u8 = 2;
 const PADDLE_WIDTH: u8 = 8;
@@ -46,8 +47,8 @@ fn main() -> ! {
 
     loop {
         let vx = match last_winner_id {
-            1 => 2,
-            _ => -2,
+            1 => 2.0,
+            _ => -2.0,
         };
 
         let mut ball = Ball::new(vx);
@@ -56,7 +57,7 @@ fn main() -> ! {
         let mut score_str = ArrayString::<[u8; 20]>::new();
         write!(&mut score_str, "{} - {}", p1_score, p2_score).expect("Can't write");
         disp.draw(
-            Font8x16::render_str(&score_str)
+            Font6x12::render_str(&score_str)
                 .with_stroke(Some(1u8.into()))
                 .translate(Coord::new(
                     (SCREEN_WIDTH as i32 / 2) - 3 * 8,
@@ -71,13 +72,15 @@ fn main() -> ! {
 
         last_winner_id = loop {
             if ball.is_at_paddle(End::Left) {
-                if ball.test_collision(&player_1) {
-                    ball.bounce();
+                let collision_position = ball.test_collision(&player_1);
+                if collision_position.abs() <= 1.0 {
+                    ball.bounce(collision_position);
                 }
             }
             if ball.is_at_paddle(End::Right) {
-                if ball.test_collision(&player_2) {
-                    ball.bounce();
+                let collision_position = ball.test_collision(&player_2);
+                if collision_position.abs() <= 1.0 {
+                    ball.bounce(collision_position);
                 }
             }
             if ball.is_at_end(End::Left) {
@@ -130,47 +133,47 @@ enum End {
 }
 
 struct Player {
-    paddle_position: u8,
+    paddle_position: f32,
     end: End,
 }
 
 impl Player {
     fn new(end: End) -> Self {
         Player {
-            paddle_position: 0,
+            paddle_position: 0.0,
             end: end,
         }
     }
 
     pub fn move_paddle_left(&mut self) {
-        let new_position = self.paddle_position as i8 - 1;
-        self.paddle_position = if new_position < 0 {
-            0
+        let new_position = self.paddle_position - 1.0;
+        self.paddle_position = if new_position < 0.0 {
+            0.0
         } else {
-            new_position as u8
+            new_position
         };
     }
 
     pub fn move_paddle_right(&mut self) {
-        let new_position = self.paddle_position as i8 + 1;
-        let limit = SCREEN_HEIGHT - PADDLE_WIDTH - 1;
-        self.paddle_position = if new_position > limit as i8 {
+        let new_position = self.paddle_position + 1.0;
+        let limit = SCREEN_HEIGHT as f32 - PADDLE_WIDTH as f32 - 1.0;
+        self.paddle_position = if new_position > limit {
             limit
         } else {
-            new_position as u8
+            new_position
         };
     }
 
     pub fn paddle_drawable(&self) -> impl Iterator<Item = Pixel<PixelColorU8>> {
         let x = match self.end {
-            End::Left => 0,
-            End::Right => SCREEN_WIDTH - PADDLE_THICKNESS,
+            End::Left => 0.0,
+            End::Right => SCREEN_WIDTH as f32 - PADDLE_THICKNESS as f32,
         };
         Rect::new(
             Coord::new(x as i32, self.paddle_position as i32),
             Coord::new(
-                (x + PADDLE_THICKNESS) as i32,
-                (self.paddle_position + PADDLE_WIDTH) as i32,
+                (x + PADDLE_THICKNESS as f32) as i32,
+                self.paddle_position.round() as i32 + PADDLE_WIDTH as i32,
             ),
         )
         .with_fill(Some(1u8.into()))
@@ -179,63 +182,69 @@ impl Player {
 }
 
 struct Ball {
-    radius: u8,
-    x: u8,
-    y: u8,
-    vx: i8,
-    vy: i8,
+    radius: f32,
+    x: f32,
+    y: f32,
+    vx: f32,
+    vy: f32,
 }
 
 impl Ball {
-    fn new(vx: i8) -> Self {
+    fn new(vx: f32) -> Self {
         Ball {
-            radius: 3,
-            x: SCREEN_WIDTH / 2,
-            y: SCREEN_HEIGHT / 2,
+            radius: 3.0,
+            x: SCREEN_WIDTH as f32 / 2.0,
+            y: SCREEN_HEIGHT as f32 / 2.0,
             vx: vx,
-            vy: 1,
+            vy: 0.1,
         }
     }
 
     fn update(&mut self) {
-        if self.y >= (SCREEN_HEIGHT - self.radius) || self.y < self.radius {
+        if self.y >= (SCREEN_HEIGHT as f32 - self.radius) || self.y < self.radius {
             self.vy = -self.vy;
         }
 
-        let new_x = self.x as i8 + self.vx;
-        let new_y = self.y as i8 + self.vy;
+        let new_x = self.x + self.vx;
+        let new_y = self.y + self.vy;
 
-        self.x = if new_x > 0 { new_x as u8 } else { 0 };
-        self.y = if new_y > 0 { new_y as u8 } else { 0 };
+        self.x = if new_x > 0.0 { new_x } else { 0.0 };
+        self.y = if new_y > 0.0 { new_y } else { 0.0 };
     }
 
-    fn test_collision(&self, player: &Player) -> bool {
-        self.y as i8 >= player.paddle_position as i8
-            && self.y as i8 <= player.paddle_position as i8 + PADDLE_WIDTH as i8 - 1
+    fn test_collision(&self, player: &Player) -> f32 {
+        let hit_area = PADDLE_WIDTH as f32 / 2.0 + self.radius / 2.0;
+        let paddle_center = player.paddle_position + hit_area;
+        let diff = paddle_center - self.y;
+        (diff / hit_area)
     }
 
     fn is_at_end(&self, end: End) -> bool {
         match end {
             End::Left => self.x < self.radius,
-            End::Right => self.x >= (SCREEN_WIDTH - self.radius),
+            End::Right => self.x >= (SCREEN_WIDTH as f32 - self.radius),
         }
     }
 
     fn is_at_paddle(&self, end: End) -> bool {
         match end {
-            End::Left => self.x < self.radius + PADDLE_THICKNESS,
-            End::Right => self.x >= (SCREEN_WIDTH - self.radius - PADDLE_THICKNESS),
+            End::Left => self.x < self.radius + PADDLE_THICKNESS as f32,
+            End::Right => self.x >= (SCREEN_WIDTH as f32 - self.radius - PADDLE_THICKNESS as f32),
         }
     }
 
-    fn bounce(&mut self) {
+    fn bounce(&mut self, collision_position: f32) {
         self.vx = -self.vx;
+        self.vy = -collision_position;
     }
 
     fn drawable(&self) -> impl Iterator<Item = Pixel<PixelColorU8>> {
-        Circle::new(Coord::new(self.x as i32, self.y as i32), self.radius as u32)
-            .with_stroke(Some(1u8.into()))
-            .into_iter()
+        Circle::new(
+            Coord::new(self.x.round() as i32, self.y.round() as i32),
+            self.radius.round() as u32,
+        )
+        .with_stroke(Some(1u8.into()))
+        .into_iter()
     }
 }
 
