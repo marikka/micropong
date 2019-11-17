3e86f62811b05b666e378321ad81a5b77cc79a72
Marek Miettinen <marek.miettinen@gmail.com>
2019-11-16 20:22:48 -0500

    10 blocks per row

--- src/tetris.rs
+++ src/tetris.rs
@@ -18,7 +18,7 @@ const SCREEN_HEIGHT: u8 = 32;
 const GRID_WIDTH: usize = 10;
 const GRID_HEIGHT: usize = 40;
 const BLOCK_SIZE: i32 = 3;
-const MARGIN_X: i32 = (SCREEN_HEIGHT as i32 - BLOCK_SIZE * GRID_WIDTH as i32) / 2;
+const MARGIN_X: i32 = (SCREEN_HEIGHT as i32 - BLOCK_SIZE * GRID_WIDTH as i32) / 2 + BLOCK_SIZE;
 const NUM_TETROMINOES: usize = 7;
 
 static I: TetrominoData = [[1, 0], [1, 0], [1, 0], [1, 0]];
@@ -42,8 +42,8 @@ enum TetrominoType {
     J = 6,
 }
 
-impl From<usize> for TetrominoType {
-    fn from(x: usize) -> Self {
+impl From<i32> for TetrominoType {
+    fn from(x: i32) -> Self {
         match x {
             0 => TetrominoType::I,
             1 => TetrominoType::T,
@@ -62,8 +62,8 @@ type Grid = [[usize; GRID_WIDTH]; GRID_HEIGHT];
 type TetrominoData = [[usize; 2]; 4];
 
 struct Tetromino {
-    x: usize,
-    y: usize,
+    x: i32,
+    y: i32,
     typ: TetrominoType,
 }
 
@@ -109,7 +109,7 @@ pub fn tetris<E: core::fmt::Debug, GM: ssd1306::interface::DisplayInterface<Erro
             break;
         }
 
-        let typ = TetrominoType::from((wyrng(&mut seed) % NUM_TETROMINOES as u64) as usize);
+        let typ = TetrominoType::from((wyrng(&mut seed) % NUM_TETROMINOES as u64) as i32);
         let mut current_tetromino = Tetromino { x: 5, y: 0, typ };
 
         loop {
@@ -124,7 +124,7 @@ pub fn tetris<E: core::fmt::Debug, GM: ssd1306::interface::DisplayInterface<Erro
             for y in 0..GRID_HEIGHT {
                 for x in 0..GRID_WIDTH {
                     if grid[y][x] > 0 {
-                        disp.draw(block_drawable(y, x));
+                        disp.draw(block_drawable(y as i32, x as i32));
                     }
                 }
             }
@@ -139,7 +139,9 @@ pub fn tetris<E: core::fmt::Debug, GM: ssd1306::interface::DisplayInterface<Erro
 
             delay.delay_ms(50u16);
 
-            if current_tetromino.y >= GRID_HEIGHT - 4 || !current_tetromino.can_move_down(&grid) {
+            if current_tetromino.y >= GRID_HEIGHT as i32 - 4
+                || !current_tetromino.can_move_down(&grid)
+            {
                 draw_tetromino_on_grid(
                     current_tetromino.x,
                     current_tetromino.y,
@@ -154,8 +156,8 @@ pub fn tetris<E: core::fmt::Debug, GM: ssd1306::interface::DisplayInterface<Erro
 }
 
 fn tetromino_drawable(
-    x0: usize,
-    y0: usize,
+    x0: i32,
+    y0: i32,
     t_type: TetrominoType,
 ) -> impl Iterator<Item = Pixel<PixelColorU8>> {
     let t = TETROMINOES[t_type as usize];
@@ -165,7 +167,7 @@ fn tetromino_drawable(
         .flat_map(|e| core::iter::repeat(e).zip((0..2).into_iter()))
         .filter_map(move |(y, x)| {
             if t[y][x] > 0 {
-                Some(block_drawable(y0 + y, x0 + x))
+                Some(block_drawable(y0 + y as i32, x0 + x as i32))
             } else {
                 None
             }
@@ -173,21 +175,21 @@ fn tetromino_drawable(
         .flat_map(|e| e)
 }
 
-fn draw_tetromino_on_grid(x0: usize, y0: usize, t_type: TetrominoType, g: &mut Grid) {
+fn draw_tetromino_on_grid(x0: i32, y0: i32, t_type: TetrominoType, g: &mut Grid) {
     let t = TETROMINOES[t_type as usize];
     for y in 0..4 {
         for x in 0..2 {
             let yi = y0 + y;
             let xi = x0 + x;
-            if yi > 0 && yi < GRID_HEIGHT && xi > 0 && xi < GRID_WIDTH {
-                g[yi][xi] |= t[y][x];
+            if yi > 0 && yi < GRID_HEIGHT as i32 && xi >= 0 && xi < GRID_WIDTH as i32 {
+                g[yi as usize][xi as usize] |= t[y as usize][x as usize];
             }
         }
     }
 }
 
-fn block_drawable(y: usize, x: usize) -> impl Iterator<Item = Pixel<PixelColorU8>> {
-    let x_display = y as i32 * BLOCK_SIZE;
+fn block_drawable(y: i32, x: i32) -> impl Iterator<Item = Pixel<PixelColorU8>> {
+    let x_display = y * BLOCK_SIZE;
     let y_display = SCREEN_HEIGHT as i32 - (x as i32 * BLOCK_SIZE) - MARGIN_X;
     Rect::new(
         Coord::new(x_display, y_display),
@@ -197,16 +199,16 @@ fn block_drawable(y: usize, x: usize) -> impl Iterator<Item = Pixel<PixelColorU8
     .into_iter()
 }
 
-fn test_collision(x0: usize, y0: usize, t_type: TetrominoType, g: &Grid) -> bool {
+fn test_collision(x0: i32, y0: i32, t_type: TetrominoType, g: &Grid) -> bool {
     let t = TETROMINOES[t_type as usize];
-    for y in 0..4 {
-        for x in 0..2 {
-            let yi = y0 + y;
-            let xi = x0 + x;
+    for y in 0..4 as usize {
+        for x in 0..2 as usize {
+            let yi = y0 + y as i32;
+            let xi = x0 + x as i32;
 
             if t[y][x] == 1 {
-                if yi > 0 && yi < GRID_HEIGHT && xi > 0 && xi < GRID_WIDTH {
-                    if g[yi][xi] == 1 {
+                if yi > 0 && yi < GRID_HEIGHT as i32 && xi >= 0 && xi < GRID_WIDTH as i32 {
+                    if g[yi as usize][xi as usize] == 1 {
                         return true;
                     }
                 } else {
